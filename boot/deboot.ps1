aa0305310adcc6f556bda41010f74d495a2fa724
joeblew999 <joeblew999@gmail.com>
2025-05-07 15:58:48 +1000

    deboot

--- boot/deboot.ps1
+++ boot/deboot.ps1
@@ -12,7 +12,7 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 # Uninstalls Bun using winget
 try {
     Write-Host "Attempting to uninstall Bun using winget..."
-    winget uninstall --id=Oven-sh.Bun -e --accept-source-agreements --accept-package-agreements --silent
+    winget uninstall --id=Oven-sh.Bun -e --accept-source-agreements --silent
     Write-Host "Bun uninstallation process finished."
 } catch {
     Write-Warning "Failed to uninstall Bun using winget. It might not have been installed or an error occurred."
@@ -21,7 +21,7 @@ try {
 # Uninstalls Git using winget
 try {
     Write-Host "Attempting to uninstall Git using winget..."
-    winget uninstall --id=Git.Git -e --accept-source-agreements --accept-package-agreements --silent
+    winget uninstall --id=Git.Git -e --accept-source-agreements --silent
     Write-Host "Git uninstallation process finished."
 } catch {
     Write-Warning "Failed to uninstall Git using winget. It might not have been installed or an error occurred."
@@ -31,7 +31,7 @@ try {
 # Uninstalls Go using winget
 try {
     Write-Host "Attempting to uninstall Go using winget..."
-    winget uninstall --id=GoLang.Go -e --accept-source-agreements --accept-package-agreements --silent
+    winget uninstall --id=GoLang.Go -e --accept-source-agreements --silent
     Write-Host "Go uninstallation process finished."
 } catch {
     Write-Warning "Failed to uninstall Go using winget. It might not have been installed or an error occurred."
@@ -41,7 +41,7 @@ try {
 # Uninstalls OpenSSH using winget
 try {
     Write-Host "Attempting to uninstall OpenSSH using winget..."
-    winget uninstall --id=Microsoft.OpenSSH.Preview -e --accept-source-agreements --accept-package-agreements --silent
+    winget uninstall --id=Microsoft.OpenSSH.Preview -e --accept-source-agreements --silent
     Write-Host "OpenSSH uninstallation process finished."
 } catch {
     Write-Warning "Failed to uninstall OpenSSH using winget. It might not have been installed or an error occurred."
@@ -51,7 +51,7 @@ try {
 # Uninstalls Which using winget
 try {
     Write-Host "Attempting to uninstall Which using winget..."
-    winget uninstall --id=GnuWin32.Which -e --accept-source-agreements --accept-package-agreements --silent
+    winget uninstall --id=GnuWin32.Which -e --accept-source-agreements --silent
     Write-Host "Which uninstallation process finished."
 } catch {
     Write-Warning "Failed to uninstall Which using winget. It might not have been installed or an error occurred."
