6074592179d2b9e60e1eb029f7d539424f7d4d70
joeblew999 <joeblew999@gmail.com>
2025-05-07 15:44:16 +1000

    boot

--- boot/boot.ps1
+++ boot/boot.ps1
@@ -1,3 +1,11 @@
+# Check Privs
+
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process powershell -Verb runAs -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"" + $PSCommandPath + "`"")
+    exit
+}
+
+
 # Installs Git using winget
 Write-Host "Attempting to install Git using winget..."
 winget install --id=Git.Git -e --accept-source-agreements --accept-package-agreements
