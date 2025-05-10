fe7443fb48d1ac678361beb7b4286297ba504758
joeblew999 <joeblew999@gmail.com>
2025-05-10 13:23:55 +1000

    aa

--- _hack/variables/run.ps1
+++ _hack/variables/run.ps1
@@ -1,3 +1,6 @@
+# Set Execution Policy to Bypass for the current session
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
 & {
     $env:HTML_DEEP_PATH = Join-Path -Path $PWD.Path -ChildPath "over-from-shell"
     $env:HTML_DEEP_NAME = "over-from-shell"
